74a6622e30f255c994954bd8142633ea8ec4ccd8
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-22 08:22:15 +0200

    Started styling page 3

--- src/components/second-page/SecondPage.tsx
+++ src/components/second-page/SecondPage.tsx
@@ -8,18 +8,20 @@ import apartmentImage from '../../assets/apartment.png'
 
 const SecondPage: React.FC = () => {
   return (
-    <div className={'second-page'} >
-      <div className={'second-page-main-row'}>
-      <div className={'second-page-top-column'}>
-        <h2>3 EASY STEPS</h2>
-        <div className={'second-page-separator'} />
+    <div className="second-page" >
+      <div className="second-page-main-row">
+      <div className="second-page-top-column">
+        <h2 className="page__title">
+          3 EASY STEPS
+        </h2>
+        <div className="page__separator" />
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
           in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
       </div>
-      <div className={'second-page-main-box'}>
+      <div className="second-page-main-box">
         <BorderAnimation />
         <MainBoxChild image={archiveImage} />
         <MainBoxChild image={tenantImage} />
--- src/components/third-page/ThirdPage.tsx
+++ src/components/third-page/ThirdPage.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ThirdPage: React.FC = () => {
   return (
-    <div className={'third-page'} >
-
+    <div className="third-page">
+      <div className="third-page__top-column">
+        <h2 className="page__title">
+          MORE BENEFITS
+        </h2>
+        <div className="page__separator"/>
+      </div>
     </div>
   )
 }
